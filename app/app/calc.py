3a2491071ccbe342e3d63a96ad436198e663b51d
Qiydaarwilliams23@gmail.com <Qiydaarwilliams23@gmail.com>
2024-08-30 18:51:30 -0400

    docker / postgres connection build

---
+++ app/app/calc.py
@@ -0,0 +1,16 @@
+"""
+Calculator functions
+"""
+
+def add(x, y):
+    """Add x and y return results."""
+    return x + y
+
+def subtract(x, y):
+    """subtract x and y return results."""
+    return x - y
+
+def multiple(x, y):
+    """multiple x and y return results"""
+    
+    return x * y
