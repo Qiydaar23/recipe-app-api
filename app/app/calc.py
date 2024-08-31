9164867b1e1d8934e7372bfa492eac1fb661f71b
Qiydaarwilliams23@gmail.com <Qiydaarwilliams23@gmail.com>
2024-08-31 09:45:57 -0400

    docker / postgres connection build

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,16 +1,15 @@
-# """
-# Calculator functions
-# """
+"""
+Calculator functions.
+"""
 
-# def add(x, y):
-#     """Add x and y return results."""
-#     return x + y
+def add(x, y):
+    """Add x and y return results."""
+    return x + y
 
-# def subtract(x, y):
-#     """subtract x and y return results."""
-#     return x - y
+def subtract(x, y):
+    """Subtract x and y return results."""
+    return x - y
 
-# def multiple(x, y):
-#     """multiple x and y return results"""
-    
-#     return x * y
+def multiple(x, y):
+    """Multiply x and y return results."""
+    return x * y
--- app/app/tests.py
+++ app/app/tests.py
@@ -1,27 +1,23 @@
-# """
-# sample tests
-# """
-# from django.test import SimpleTestCase
+"""
+Sample tests.
+"""
+from django.test import SimpleTestCase
+from . import calc
 
-# from . import calc
+class CalcTests(SimpleTestCase):
+    """Test the calc module."""
 
-# class CalcTests(SimpleTestCase):
-#     """Test the calc module"""
-    
-#     def test_add_numbers(self):
-#         """ Test adding numbers together"""
-#         res = calc.add(5, 6)
-        
-#         self.assertEqual(res,11)
-        
-#     def test_subtract_numbers(self):
-#         """test subtracting numbers."""
-#         res = calc.subtract(15, 10)
-        
-#         self.assertEqual(res, 5)
-        
-#     def test_multiple_numbers(self):
-#         """test multiplying numbers"""
-#         res = calc.multiple(8, 2)
-        
-#         self.assertEqual(res, 16)
+    def test_add_numbers(self):
+        """Test adding numbers together."""
+        res = calc.add(5, 6)
+        self.assertEqual(res, 11)
+
+    def test_subtract_numbers(self):
+        """Test subtracting numbers."""
+        res = calc.subtract(15, 10)
+        self.assertEqual(res, 5)
+
+    def test_multiple_numbers(self):
+        """Test multiplying numbers."""
+        res = calc.multiple(8, 2)
+        self.assertEqual(res, 16)
