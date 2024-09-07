22e040f1776436519e5a4239d18f59f2a9546056
Qiydaarwilliams23@gmail.com <Qiydaarwilliams23@gmail.com>
2024-09-07 13:11:23 -0400

    code refactored

--- app/recipe/tests/test_ingredients_api.py
+++ app/recipe/tests/test_ingredients_api.py
@@ -2,11 +2,11 @@
 Test for the inredients API
 """
 
-from django.contrib import get_user_model
+from django.contrib.auth import get_user_model
 from django.urls import reverse
 from django.test import TestCase
 
-from test_framework import status
+from rest_framework import status
 from rest_framework.test import APIClient
 
 from core.models import Ingredient
@@ -14,6 +14,10 @@ from core.models import Ingredient
 from recipe.serializers import IngredientSerializer
 
 INGREDIENTS_URL = reverse('recipe:ingredient-list')
+
+def detail_url(ingredient_id):
+    """Create and return an ingredient detail URL"""
+    return reverse('recipe:ingredient-detail', args=[ingredient_id])
 
 def create_user(email='user@example.com', password='testpass123'):
     """Create and return user"""
@@ -33,7 +37,7 @@ class PublicIngredientsApiTests(TestCase):
 class PrivateIngredientsApiTests(TestCase):
     """Test unautheticated API requests"""
 
-    def seyUp(self):
+    def setUp(self):
         self.user = create_user()
         self.client = APIClient()
         self.client.force_authenticate(self.user)
@@ -50,15 +54,39 @@ class PrivateIngredientsApiTests(TestCase):
         self.assertEqual(res.data, serializer.data)
 
 
-    def yesy_ingredients_limited_to_user(self):
+    def test_ingredients_limited_to_user(self):
         """Test list of ingredients is limited to authenticated user"""
         user2 = create_user(email='user2@example.com')
         Ingredient.objects.create(user=user2, name='Salt')
         ingredient = Ingredient.objects.create(user=self.user, name='Pepper')
 
-        res = self.client.get(Ingredient)
+        res = self.client.get(INGREDIENTS_URL)
 
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]['name'], ingredient.name)
         self.assertEqual(res.data[0]['id'], ingredient.id)
+
+    def test_update_ingredient(self):
+        """Test updating an ingredient"""
+        ingredient = Ingredient.objects.create(user=self.user, name='Cilantro')
+
+        payload = {'name':'Coriander'}
+        url = detail_url(ingredient.id)
+        res = self.client.patch(url, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        ingredient.refresh_from_db()
+        self.assertEqual(ingredient.name, payload['name'])
+
+
+    def test_delete_ingredient(self):
+        """Test deleting an ingredient"""
+        ingredient = Ingredient.objects.create(user=self.user, name='Lettuce')
+
+        url  = detail_url(ingredient.id)
+        res = self.client.delete(url)
+
+        self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
+        ingredients = Ingredient.objects.filter(user=self.user)
+        self.assertFalse(ingredients.exists())
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -5,7 +5,7 @@ from rest_framework import viewsets ,  mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Recipe, Tag
+from core.models import Recipe, Tag , Ingredient
 from recipe import serializers
 
 class RecipeViewSet(viewsets.ModelViewSet):
@@ -30,16 +30,38 @@ class RecipeViewSet(viewsets.ModelViewSet):
         """Create a new recipe."""
         serializer.save(user=self.request.user)
 
-class TagViewSet(mixins.DestroyModelMixin,mixins.UpdateModelMixin,mixins.ListModelMixin, viewsets.GenericViewSet):
+
+
+class BaseRecipeAttrViewSet(mixins.DestroyModelMixin,mixins.UpdateModelMixin,mixins.ListModelMixin, viewsets.GenericViewSet):
+    """Base Viewset for aatributes"""
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+           """Filter queryset to authenticated user"""
+           return self.queryset.filter(user=self.request.user).order_by('-name')
+
+class TagViewSet(BaseRecipeAttrViewSet):
        """Manage tags in the database"""
        serializer_class = serializers.TagSerializer
        queryset = Tag.objects.all()
-       authentication_classes = [TokenAuthentication]
-       permission_classes = [IsAuthenticated]
+    #    authentication_classes = [TokenAuthentication]
+    #    permission_classes = [IsAuthenticated]
 
-       def get_queryset(self):
-           """Filter queryset to authenticated user"""
-           return self.queryset.filter(user=self.request.user).order_by('-name')
+    #    def get_queryset(self):
+    #        """Filter queryset to authenticated user"""
+    #        return self.queryset.filter(user=self.request.user).order_by('-name')
+
+class IngredientViewSet(BaseRecipeAttrViewSet):
+    """Manage ingredients in the database."""
+    serializer_class = serializers.IngredientSerializer
+    queryset = Ingredient.objects.all()
+    # authentication_classes = [TokenAuthentication]
+    # permission_classes = [IsAuthenticated]
+
+    # def get_queryset(self):
+    #        """Filter queryset to authenticated user"""
+    #        return self.queryset.filter(user=self.request.user).order_by('-name')
 
 
 
